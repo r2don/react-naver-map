da2b8afb8aa3b0814d6152dc85d510ef98527bcf
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2022-06-26 21:26:17 +0900

    feat: marker (#6)
    * feat: marker
    
    * fix: deps
    
    * fix: onClick as optional & add tsdoc

---
+++ src/components/Marker.tsx
@@ -0,0 +1,44 @@
+import { useMapContext } from "src/context";
+import { useIsomorphicLayoutEffect } from "src/hooks/useIsomorphicLayoutEffect";
+import { LatLng } from "src/interfaces/LatLng";
+
+interface MarkerProps
+  extends Omit<naver.maps.MarkerOptions, "position" | "map" | "clickable"> {
+  position: LatLng;
+  onClick?: () => void;
+}
+/**
+ * A marker component for map
+ *
+ * You can customize this marker with icon prop, see more detail here {@link https://navermaps.github.io/maps.js.ncp/docs/naver.maps.Marker.html#toc37__anchor}
+ */
+export const Marker = ({
+  position: { latitude, longitude },
+  onClick,
+  ...rest
+}: MarkerProps) => {
+  const map = useMapContext();
+
+  useIsomorphicLayoutEffect(() => {
+    const marker = new naver.maps.Marker({
+      map,
+      position: new naver.maps.LatLng(latitude, longitude),
+      clickable: !!onClick,
+      ...rest,
+    });
+
+    let listener: naver.maps.MapEventListener;
+    if (onClick) {
+      listener = marker.addListener("click", onClick);
+    }
+
+    return () => {
+      if (listener) {
+        marker.removeListener(listener);
+      }
+      marker.setMap(null);
+    };
+  }, [latitude, longitude, map, rest]);
+
+  return null;
+};
