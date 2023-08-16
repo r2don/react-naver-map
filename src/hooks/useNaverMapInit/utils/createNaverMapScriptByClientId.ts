6685b93f6859743d0f65a57411b8b4fe0a5bda34
Lee Eun Kyu <95684796+yeskyu12@users.noreply.github.com>
2023-08-16 22:00:25 +0900

    feat: add submodules to InitParams (#47)
    Co-authored-by: Seongcheol Jo <48130830+josworks27@users.noreply.github.com>

--- src/hooks/useNaverMapInit/utils/createNaverMapScriptByClientId.ts
+++ src/hooks/useNaverMapInit/utils/createNaverMapScriptByClientId.ts
@@ -1,10 +1,17 @@
 import { SCRIPT_ID } from "../constants";
 import type { InitParams } from "../types";
 
-export const createNaverMapScriptByClientId = (
-  ncpClientId: InitParams["ncpClientId"],
-) => {
+export const createNaverMapScriptByClientId = ({
+  ncpClientId,
+  submodules,
+}: Pick<InitParams, "ncpClientId" | "submodules">) => {
   const script = document.createElement("script");
+  let paramsString = `ncpClientId=${ncpClientId}`;
+
+  if (submodules?.length) {
+    paramsString = paramsString.concat(`&submodules=${submodules.join(",")}`);
+  }
+
   script.id = SCRIPT_ID;
   script.type = "text/javascript";
   script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${ncpClientId}`;
